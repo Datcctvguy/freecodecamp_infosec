582e982406b6c4c1ccbd2bec0cfbed713783a30e
Datcctvguy <datcctvguy@gmail.com>
2023-04-16 19:49:21 +0100

    fixed port issues

--- myApp.js
+++ myApp.js
@@ -2,51 +2,6 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
-
-
-app.listen(PORT, () => {
-  console.log(`my freecodecamp info security application server started on port ${PORT}`);
-})
-;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
